9240e4f01707d704871cab55a48ffce9f3f59137
nerochiu <nerochiu@tl111501neor-macbook-air.local>
2015-12-17 13:55:31 +0800

    Initial Commit

---
+++ Tactri/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tactri
+//
+//  Created by nerochiu on 2015/12/17.
+//  Copyright © 2015年 Iscom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tactri/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tactri
+//
+//  Created by nerochiu on 2015/12/17.
+//  Copyright © 2015年 Iscom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
